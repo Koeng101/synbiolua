36695ea809b6ffc291dfc6420f94bbe399dcb400
Keoni Gandall <koeng101@gmail.com>
2022-07-04 17:51:13 -0700

    Add coverage checker

---
+++ cov.sh
@@ -0,0 +1,19 @@
+# Generate tl -> lua
+tl gen io/fasta/fasta.tl io/fasta/fasta_test.tl
+tl gen io/genbank/genbank.tl io/genbank/genbank_test.tl
+
+# Get stats
+lua -lluacov fasta.lua
+lua -lluacov fasta_test.lua
+lua -lluacov genbank.lua
+lua -lluacov genbank_test.lua
+luacov
+rm luacov.stats.out
+
+# run tests
+luajit fasta_test.lua
+luajit genbank_test.lua
+
+# Cleanup lua
+rm fasta.lua fasta_test.lua
+rm genbank.lua genbank_test.lua
